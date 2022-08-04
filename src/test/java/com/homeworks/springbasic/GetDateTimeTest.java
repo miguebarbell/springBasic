01dd7c693033d4424e8c40662bac3eb84c2d45e7
Miguel R <miguel@debloat.us>
2022-08-04 08:58:40 -0400

    Comparing using localdatetime and string

--- src/test/java/com/homeworks/springbasic/GetDateTimeTest.java
+++ src/test/java/com/homeworks/springbasic/GetDateTimeTest.java
@@ -2,11 +2,8 @@ package com.homeworks.springbasic;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
+import java.time.*;
 import java.util.TimeZone;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,26 +11,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GetDateTimeTest {
 	SpringBasicApplication.BasicController appTest = new SpringBasicApplication.BasicController();
+
+	/**
+	 * check the UTC from the request and the computer be equal
+	 */
 	@Test
-	@DisplayName("Zulu time test")
+	@DisplayName("UTC")
 	void getZuluTime() {
 		String zuluTimeApp = appTest.getUTC();
 		String zuluTimeTest = OffsetDateTime.now(ZoneOffset.UTC).toString();
+		// comparing strings
 		assertEquals(zuluTimeApp.split("\\.")[0], zuluTimeTest.split("\\.")[0]);
+		// comparing LocalDateTime
+		LocalDateTime ldtApp = LocalDateTime.parse(appTest.getUTC().replace("Z", ""));
+		LocalDateTime ldtTest = LocalDateTime.parse(zuluTimeTest.replace("Z", ""));
+		assertTrue(0 >= Duration.between(ldtApp, ldtTest).getSeconds());
 	}
 
+	/**
+	 * check the difference between <strong>two LocalDateTime</strong> objects be less than a second.
+	 */
 	@Test
-	@DisplayName("Eastern Time test")
+	@DisplayName("Eastern Time")
 	void getEasternTime() {
 		GetDateTime newyorkTime = new GetDateTime("America/New_York");
-		String newyorkTimeApp = newyorkTime.getTzone();
-		String newyorkTimeTest = LocalDateTime.now(TimeZone.getTimeZone("America/New_York").toZoneId()).toString();
-		assertEquals(newyorkTimeTest.split("\\.")[0], newyorkTimeApp.split("\\.")[0]);
-
+		LocalDateTime newyorkTimeApp = LocalDateTime.parse(newyorkTime.getTzone());
+		LocalDateTime newyorkTimeTest = LocalDateTime.now(TimeZone.getTimeZone("America/New_York").toZoneId());
+		assertTrue(0 >= Duration.between(newyorkTimeApp, newyorkTimeTest).getSeconds());
 	}
 
+	/**
+	 * check the similarity of <strong>two Strings</strong> until the second.
+	 */
 	@Test
-	@DisplayName("California Time test")
+	@DisplayName("California Time")
 	void getCaliforniaTime() {
 		GetDateTime newyorkTime = new GetDateTime("PST");
 		String newyorkTimeApp = newyorkTime.getTzone();
